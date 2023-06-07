cf9b66c9dd5e2d90227c21233a15faf305ff724a
MarcoKoen <neokocram@gmail.com>
2023-06-08 09:30:30 +1200

    Player movement finished

--- MazeSpooky/Assets/PlayerMovement.cs
+++ MazeSpooky/Assets/PlayerMovement.cs
@@ -9,12 +9,24 @@ public class PlayerMovement : MonoBehaviour
 
     Vector2 movement;
 
+    public Animator animator;
+
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
 
+        if (movement.x < 0)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else {
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        } 
     }
 
     private void FixedUpdate()
