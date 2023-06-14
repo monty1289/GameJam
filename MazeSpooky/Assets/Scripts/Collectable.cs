24c0f7c78a755b5587502975a866bae75cf983f5
MarcoKoen <neokocram@gmail.com>
2023-06-15 09:37:29 +1200

    Added audio clip

---
+++ MazeSpooky/Assets/Scripts/Collectable.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    public int lightValue = 1;
+    public AudioClip clip;
+    public GameObject player;
+
+    public void OnTriggerEnter2D(Collider2D other){
+
+        if(player !=null)
+        {
+            player.GetComponent<PlayerMovement>().lightValue += lightValue;
+            AudioManager.Instance.Play(clip, player.transform);
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
