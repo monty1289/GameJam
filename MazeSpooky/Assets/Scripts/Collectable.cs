dd1e2ed8f10f226cee4ed6e7382d6deaddf457cf
monty1289 <monty.wood1289@gmail.com>
2023-06-20 15:33:42 +1200

    added imame for when player loses

--- MazeSpooky/Assets/Scripts/Collectable.cs
+++ MazeSpooky/Assets/Scripts/Collectable.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Collectable : MonoBehaviour
 {
@@ -7,16 +8,31 @@ public class Collectable : MonoBehaviour
     public GameObject player;
     public GameObject spotlight;
     private float decreaseRate = 0.02f; // Rate at which the spotlight decreases in size per second
+    public Image image;
+    private float activationRadius = 0.8f;
 
     private UnityEngine.Rendering.Universal.Light2D light2D;
     private float initialOuterRadius;
     private float initialInnerRadius;
+
+    private CanvasGroup canvasGroup;
+    private float alphaIncrement = 0.2f; // Increment value for alpha increase
 
     private void Start()
     {
         light2D = spotlight.GetComponent<UnityEngine.Rendering.Universal.Light2D>();
         initialOuterRadius = light2D.pointLightOuterRadius;
         initialInnerRadius = light2D.pointLightInnerRadius;
+
+        // Get the CanvasGroup component or add it if it doesn't exist
+        canvasGroup = image.GetComponent<CanvasGroup>();
+        if (canvasGroup == null)
+        {
+            canvasGroup = image.gameObject.AddComponent<CanvasGroup>();
+        }
+
+        // Set initial alpha to 0
+        canvasGroup.alpha = 0f;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -30,7 +46,7 @@ public class Collectable : MonoBehaviour
             if (light2D != null)
             {
                 light2D.pointLightOuterRadius += 2f;
-                light2D.pointLightInnerRadius += 2f;
+                light2D.pointLightInnerRadius += 2f;                
             }
 
             Destroy(gameObject);
@@ -39,6 +55,7 @@ public class Collectable : MonoBehaviour
 
     private void Update()
     {
+        Debug.Log("Outer Radius: " + light2D.pointLightOuterRadius);
         // Decrease the spotlight's inner and outer radius over time
         if (light2D != null)
         {
@@ -47,6 +64,15 @@ public class Collectable : MonoBehaviour
 
             light2D.pointLightOuterRadius = Mathf.Clamp(newOuterRadius, 0f, initialOuterRadius);
             light2D.pointLightInnerRadius = Mathf.Clamp(newInnerRadius, 0f, initialInnerRadius);
+            
+            // Activate the image if it's not already active and the condition is met
+            if (light2D.pointLightOuterRadius <= activationRadius && canvasGroup.alpha < 1f)
+            {
+                // Gradually increase the alpha of the image over time with a slower increment
+                canvasGroup.alpha += alphaIncrement * Time.deltaTime;
+
+                Debug.Log("Image Alpha: " + canvasGroup.alpha);
+            }
         }
     }
 }
