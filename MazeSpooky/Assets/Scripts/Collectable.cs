fd8fc2fb44b851c6ac5933ecaa2c7295226f0c9f
CalebStephens <calebtaylorstephens@gmail.com>
2023-06-20 15:06:44 +1200

    Revert "Merge pull request #25 from monty1289/decreasingSpotlight"
    This reverts commit 857d30c581ac9b1f630391e476d71d40b71c2b66, reversing
    changes made to cbd22d6318be71dc6847950275f129a4c6ef6a2d.

--- MazeSpooky/Assets/Scripts/Collectable.cs
+++ MazeSpooky/Assets/Scripts/Collectable.cs
@@ -1,52 +1,43 @@
 using UnityEngine;
 
+using System.Collections;
+using System.Collections.Generic;
+
 public class Collectable : MonoBehaviour
 {
     public int lightValue = 1;
     public AudioClip clip;
     public GameObject player;
     public GameObject spotlight;
-    private float decreaseRate = 0.02f; // Rate at which the spotlight decreases in size per second
 
-    private UnityEngine.Rendering.Universal.Light2D light2D;
-    private float initialOuterRadius;
-    private float initialInnerRadius;
-
-    private void Start()
-    {
-        light2D = spotlight.GetComponent<UnityEngine.Rendering.Universal.Light2D>();
-        initialOuterRadius = light2D.pointLightOuterRadius;
-        initialInnerRadius = light2D.pointLightInnerRadius;
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
+    public void OnTriggerEnter2D(Collider2D other)
     {
         if (player != null)
         {
             player.GetComponent<PlayerMovement>().lightValue += lightValue;
             AudioManager.Instance.Play(clip, player.transform);
+            UnityEngine.Rendering.Universal.Light2D light2D = spotlight.GetComponent<UnityEngine.Rendering.Universal.Light2D>();
 
-            // Increase the spotlight's outer radius
             if (light2D != null)
             {
-                light2D.pointLightOuterRadius += 1.5f;
-                light2D.pointLightInnerRadius += 1.5f;
+                light2D.pointLightOuterRadius += 0.5f;
             }
 
             Destroy(gameObject);
         }
     }
 
-    private void Update()
+    // Start is called before the first frame update
+    void Start()
     {
-        // Decrease the spotlight's inner and outer radius over time
-        if (light2D != null)
-        {
-            float newOuterRadius = light2D.pointLightOuterRadius - (decreaseRate * Time.deltaTime);
-            float newInnerRadius = light2D.pointLightInnerRadius - (decreaseRate * Time.deltaTime);
 
-            light2D.pointLightOuterRadius = Mathf.Clamp(newOuterRadius, 0f, initialOuterRadius);
-            light2D.pointLightInnerRadius = Mathf.Clamp(newInnerRadius, 0f, initialInnerRadius);
-        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
     }
 }
+
